1d48c991a07e1cd385d81bcc6ff5d70dd5810755
erik <samenai@pm.me>
2025-05-11 16:29:25 -0400

    adding extensions to brave

--- home/programs/app/brave.nix
+++ home/programs/app/brave.nix
@@ -1,5 +1,16 @@
+{ pkgs, ... }:
 {
   programs.brave = {
     enable = true;
+    package = pkgs.brave;
+    extensions = [
+      { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock origin
+      { id = "nngceckbapebfimnlniiiahkandclblb"; } # bitwarden password manager
+      { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; } # dark reader
+
+    ];
+    commandLineArgs = [
+      "--enable-features=AcceleratedVideoDecodeLinuxZeroCopyGL,AcceleratedVideoDecodeLinuxGL,AcceleratedVideoEncoder"
+    ];
   };
 }

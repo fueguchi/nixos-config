22c70bf60b8bbe6c243f027e85a23d34f3a874dc
erik <samenai@pm.me>
2025-05-13 09:41:42 -0400

    adding extesion to brave.nix

--- home/programs/app/brave.nix
+++ home/programs/app/brave.nix
@@ -7,10 +7,10 @@
       { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock origin
       { id = "nngceckbapebfimnlniiiahkandclblb"; } # bitwarden password manager
       { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; } # dark reader
-
+      { id = "dlnejlppicbjfcfcedcflplfjajinajd"; } # bonjour startpage
     ];
     commandLineArgs = [
-      "--enable-features=AcceleratedVideoDecodeLinuxZeroCopyGL,AcceleratedVideoDecodeLinuxGL,AcceleratedVideoEncoder"
+      "--enable-features=AcceleratedVideoDecodeLinuxZeroCopyGL,AcceleratedVideoDecodeLinuxGL,AcceleratedVideoEncoder" # make some videos being video encoded
     ];
   };
 }

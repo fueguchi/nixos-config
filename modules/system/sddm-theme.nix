ab6bf85c2e3eca8c750a0db1614575dbde1c4efc
erik <samenai@pm.me>
2025-05-14 14:58:13 -0400

    trying to setup sddm theme

--- modules/system/sddm-theme.nix
+++ modules/system/sddm-theme.nix
@@ -2,11 +2,11 @@
 
 pkgs.stdenv.mkDerivation {
   name = "sddm-theme";
-  src = pkgs.fetchFromGithub {
+  src = pkgs.fetchFromGitHub {
     owner = "stepanzubkov";
     repo = "where-is-my-sddm-theme";
     rev = "025705b3296d478e74d1041e4495e7a4ae466f92";
-    sha256 = "01hmnskkkri2vxxp01yj270l9fnyngdlazvv3ip8hmhhv5xm0faa";
+    sha256 = "sha256-RIVvgPeRYZhnwSKqocZ0b8BgA5a1YVU8IZD81Hc8t5s=";
   };
   installPhase = ''
     mkdir -p $out
--- modules/system/sddm.nix
+++ modules/system/sddm.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }:
 {
+  environment.systemPackages = with pkgs; [
+    libsForQt5.qt5.qtquickcontrols2
+    libsForQt5.qt5.qtgraphicaleffects
+  ];
   services = {
     displayManager.sddm = {
       enable = true;

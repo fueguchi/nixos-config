12f1081c2b7dd8842ffa45e9ae8e025824a23622
erik <samenai@pm.me>
2025-05-24 22:03:05 -0400

    updating brave conf, will improve soon

--- home/programs/app/brave.nix
+++ home/programs/app/brave.nix
@@ -1,16 +1,21 @@
 { pkgs, ... }:
 {
-  programs.brave = {
+  programs.chromium = {
     enable = true;
-    package = pkgs.brave;
+    package = pkgs.brave; # redundancy for some reason idk 
     extensions = [
       { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock origin
       { id = "nngceckbapebfimnlniiiahkandclblb"; } # bitwarden password manager
       { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; } # dark reader
       { id = "dlnejlppicbjfcfcedcflplfjajinajd"; } # bonjour startpage
-    ];
+    ]; 
+
     commandLineArgs = [
+      # enable features
       "--enable-features=AcceleratedVideoDecodeLinuxZeroCopyGL,AcceleratedVideoDecodeLinuxGL,AcceleratedVideoEncoder" # make some videos being video encoded
+      
+      # disable features
+      "--disable-features=AutofillSavePaymentMethods"
     ];
   };
 }

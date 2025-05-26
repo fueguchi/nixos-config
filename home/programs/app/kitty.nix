05a92a3c848b5dff4a5ca5d222551b44f287be3c
erik <samenai@pm.me>
2025-05-26 14:44:16 -0400

    updating kitty font

--- home/programs/app/kitty.nix
+++ home/programs/app/kitty.nix
@@ -9,7 +9,7 @@
     '';
     settings = {
       confirm_os_window_close = 0;
-      font_family = "Maple Mono SC NF";
+      font_family = "Maple Mono";
       window_margin_width = 18;
       bold_font = "auto";
       italic_font = "auto";

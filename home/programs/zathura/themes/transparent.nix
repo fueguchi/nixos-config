2586f9ba228875297c264cb35162d343b1f359c0
erik <samenai@pm.me>
2025-05-26 14:43:17 -0400

    zathura config

---
+++ home/programs/zathura/themes/transparent.nix
@@ -0,0 +1,9 @@
+{
+  programs.zathura.options = {
+    recolor = true;
+    default-bg = "rgba(0,0,0,0.8)";
+    recolor-lightcolor = "rgba(0,0,0,0)";
+    adjust-open = "width";
+    font = "Maple Mono Bold"
+  };
+}

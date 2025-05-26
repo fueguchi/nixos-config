79cc000688f078963cc1539b06e0d38048f02b54
erik <samenai@pm.me>
2025-05-26 14:43:45 -0400

    updating rules to hyprland 0.49.0

--- home/desktop/hyprland/config-1/rules.nix
+++ home/desktop/hyprland/config-1/rules.nix
@@ -1,62 +1,46 @@
 {
   wayland.windowManager.hyprland.settings = {
     windowrule = [
-      "float,(org.pwmt.zathura)"
-      "float,(blueman-manager-wrapped)"
-      "float,(.protonvpn-app-wrapped)"
-      "float,(org.pulseaudio.pavucontrol)"
-      "suppressevent maximize, class:.*"
-    ];
-    windowrulev2 = [
       # fix some dragging issuse with xwayland
       "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
-
+      
+      # idk but hyprland recommends in default conf
+      "suppressevent maximize, class:.*"
       # zathura
-      "center,class:^(org.pwmt.zathura)$"
-      "size 985 960,class:^(org.pwmt.zathura)$"
+      "float, class: org.pwmt.zathura"
+      "center, class: org.pwmt.zathura"
+      "size 985 960, class: org.pwmt.zathura"
 
       # bluetooth manager
-      "center,class:^(.blueman-manager-wrapped)$,title:^(Bluetooth)$"
-      "float,class:^(.blueman-manager-wrapped)$,title:^(Bluetooth)$"
-      "size 1150 750,class:^(.blueman-manager-wrapped)$,title:^(Bluetooth)$"
+      "center, class: .blueman-manager-wrapped, title: Bluetooth"
+      "float, class: .blueman-manager-wrapped, title: Bluetooth"
+      "size 1150 750, class: .blueman-manager-wrapped, title: Bluetooth"
 
       # nautilus
-      "center,class:^(org.gnome.Nautilus)$,title:^(Loading...)$"
-      "float,class:^(org.gnome.Nautilus)$,title:^(Loading...)$"
-      "size 1150 750,class:^(org.gnome.Nautilus)$,title:^(Loading...)$"
+      "center, class: org.gnome.Nautilus"
+      "float, class: org.gnome.Nautilus"
+      "size 1150 750, class: org.gnome.Nautilus"
 
       # gtk file chooser
-      "float,class:(xdg-desktop-portal-gtk),title:^(.*Files)$"
-      "center,class:(xdg-desktop-portal-gtk),title:^(.*Files)$"
-      "size 1150 750,class:(xdg-desktop-portal-gtk),title:^(.*Files)$"
-      "float,class:(xdg-desktop-portal-gtk),title:^(.*Open)$"
-      "center,class:(xdg-desktop-portal-gtk),title:^(.*Open)$"
-      "size 1150 750,class:(xdg-desktop-portal-gtk),title:^(.*Open)$"
-      "float,class:(xdg-desktop-portal-gtk),title:^(.*Save)$"
-      "center,class:(xdg-desktop-portal-gtk),title:^(.*Save)$"
-      "size 1150 750,class:(xdg-desktop-portal-gtk),title:^(.*Save)$"
-      "float,class:(xdg-desktop-portal-gtk),title:^(.*discord.com)$"
-      "size 1150 750,class:(xdg-desktop-portal-gtk),title:^(.*discord.com)$"
-      "center,class:(xdg-desktop-portal-gtk),title:^(.*discord.com)$"
+      "float, class: xdg-desktop-portal-gtk"
+      "center, class: xdg-desktop-portal-gtk"
+      "size 1150 750, class: xdg-desktop-portal-gtk"
 
       # protonvpn gtk
-      "float,class:(.protonvpn-app-wrapped),title:^(Proton)$"
-      "size 400 600,class:^(.protonvpn-app-wrapped)$,title:^(Proton)$"
-      "center,class:^(.protonvpn-app-wrapped)$,title:^(Proton)$"
+      "float, class: .protonvpn-app-wrapped, title: Proton"
+      "size 400 600, class: .protonvpn-app-wrapped, title: Proton"
+      "center, class: .protonvpn-app-wrapped, title: Proton"
 
       # discord file chooser
-      "float,class:^(discord.*)$,title:^(Open.*)$"
+      "float, class:discord, initialTitle:Open"
 
       # waypaper
       "center,class:waypaper,title:waypaper"
 
       # pavucontrol
-      "size 750 400,class:^(org.pulseaudio.pavucontrol),title:^(.*Volume)$"
-      "center,class:^(org.pulseaudio.pavucontrol),title:^(.*Volume)$"
-
-      # steam
-      "float,class:(steam),title:^(Steam)$"
-      "center,class:(steam),title^(Steam)$"
+      "float, class: org.pulseaudio.pavucontrol"
+      "size 750 400, class: org.pulseaudio.pavucontrol"
+      "center, class: org.pulseaudio.pavucontrol"
     ];
 
     layerrule = [

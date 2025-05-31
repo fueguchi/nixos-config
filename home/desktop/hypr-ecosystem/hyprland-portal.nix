4bfbba0a001d529c4757eb0eeffbf57c41370412
erik <samenai@pm.me>
2025-05-31 09:53:38 -0400

    hypr-ecosystem correction

--- home/desktop/hypr-ecosystem/hyprland-portal.nix
+++ home/desktop/hypr-ecosystem/hyprland-portal.nix
@@ -2,6 +2,6 @@
   xdg.configFile."./config/xdg-desktop-portal/hyprland-portals.conf".text = ''
     [preferred]
     default = hyprland;gtk
-    org.freedesktop.impl.portal.FileChooser = yazi
+    org.freedesktop.impl.portal.FileChooser = nautilus
   '';
 }

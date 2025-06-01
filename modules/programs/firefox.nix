b50a3b167f70410587c9531c06d31b16a17e4273
erik <samenai@pm.me>
2025-06-01 15:27:39 -0400

    trying to setup firefox

--- modules/programs/firefox.nix
+++ modules/programs/firefox.nix
@@ -2,27 +2,5 @@
 {
   programs.firefox = {
     enable = true;
-
-    policies = {
-      DisableTelemetry = true;
-      DisableFirefoxStudies = true;
-      DontCheckDefaultBrowser = true;
-      DisablePocket = true;
-
-      ExtensionsSettings = {
-        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/file/4493940/bitwarden_password_manager-2025.5.0.xpi";
-          installation_mode = "force_installed";
-        };
-        "uBlock0@raymondhill.net" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/file/4492375/ublock_origin-1.64.0.xpi";
-          installation_mode = "force_installed";
-        };
-        "addon@darkreader.org" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/file/4488139/darkreader-4.9.106.xpi";
-          installation_mode = "force_installed";
-        };
-      };
-    };
   };
 }

2fa9b13dceb1ac85036e592dd708e83591a2f975
erik <samenai@pm.me>
2025-06-01 11:33:42 -0400

    adding gamescope

--- modules/gaming/steam.nix
+++ modules/gaming/steam.nix
@@ -3,15 +3,18 @@
   programs = {
     steam = {
       enable = true;
-      #gamescopeSession.enable = true;
+      gamescopeSession.enable = true;
       remotePlay.openFirewall = true;
       dedicatedServer.openFirewall = true;
     };
     gamemode.enable = true;
-    #gamecospe = { # not working well on NixOS 24.11
-    #  enable = true;
-    #  capSysNice = true;
-    #};
+    gamescope = { 
+      enable = true;
+      #capSysNice = true;
+    };
   };
+  environment.systemPackages = with pkgs; [
+    gamescope-wsi
+  ];
 }
  

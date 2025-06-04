e1e7dbf973faa67ee500b6b5863009bffcee64b5
erik <samenai@pm.me>
2025-06-04 14:14:29 -0400

    just changing packages to another modules

--- modules/programs/cli.nix
+++ modules/programs/cli.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
+    pywal16
     btop
     eza
   ];
--- modules/programs/misc/default.nix
+++ modules/programs/misc/default.nix
@@ -1,12 +1,8 @@
 { pkgs, pkgs-unstable, ... }:
 {
   imports = [ ./eww.nix ];
-  programs = {
-    noisetorch.enable = true;
-  };
   environment.systemPackages = (with pkgs; [
     rofi
-    pywal
     ffmpeg-full
   ]) ++ (with pkgs-unstable; [
     spotify

bd09b821d632ef3db88e22ef2dd4dc7c75f497ca
erik <samenai@pm.me>
2025-07-07 17:18:05 -0400

    add: changes in nixvim

--- modules/home/desktop/nixvim/keybinds/default.nix
+++ modules/home/desktop/nixvim/keybinds/default.nix
@@ -1,5 +1,35 @@
 {
-  programs.nixvim = {
-    globals.mapleader = "space";
-  };
+programs.nixvim.globals.mapleader = " ";
+programs.nixvim.keymaps = [
+    {
+      mode = "n";       
+      key = "<leader>q";
+      action = ":NvimTreeToggle<CR>";
+      options = {
+        noremap = true;
+        silent = true;
+        desc = "Toggle NvimTree";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>w";
+      action = ":NvimTreeFocus<CR>";
+      options = {
+        noremap = true;
+        silent = true;
+        desc = "Focus NvimTree";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>e";
+      action = "<C-w>p";
+      options = {
+        noremap = true;
+        silent = true;
+        desc = "Focus previous window";
+      };
+    }
+  ];
 }

a27fbd3266021e6109554cfc21757faff5c3003c
erik <samenai@pm.me>
2025-07-09 13:50:19 -0400

    style: visual changes in zathura + wal selector

--- modules/home/desktop/zathura/themes/transparent.nix
+++ modules/home/desktop/zathura/themes/transparent.nix
@@ -1,10 +1,10 @@
 {
   programs.zathura.options = {
     recolor = true;
-    default-bg = "rgba(10,10,10,1)";
+    default-bg = "rgba(0,0,0,0.7)";
     recolor-lightcolor = "rgba(0,0,0,0)";
     adjust-open = "width";
-    font = "Maple Mono Bold";
-    guioptions = ""; # remove the bar that shows page and the file name
+    guioptions = ""; 
+    first-page-column = "1:2";
   };
 }

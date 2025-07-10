8f905014fa48fc5059aa53ba9b41e381c1ae9dc9
erik <samenai@pm.me>
2025-07-10 00:14:26 -0400

    update: configuration.nix

--- host/wired/configuration.nix
+++ host/wired/configuration.nix
@@ -7,10 +7,17 @@
 
   intel.enable = true;
   laptop.enable = true;
-  keyring.enable = true; # if you use a DE just disable this
-  gaming.enable = true;
-  steam.enable = false;
+  keyring.enable = true; 
   vm.enable = false;
+  gaming = {
+    osu.enable = true;
+    steam.enable = true;
+    r2mod.enable = true;
+    heroic.enable = true;
+    gamemode.enable = true;
+    minecraft.enable = false;
+  };
+
 
   users.users.erik = {
     isNormalUser = true;
@@ -20,6 +27,7 @@
 
   console.keyMap = "br-abnt2";
   programs.git.enable = true;
-  programs.git.config.credential.helper = "libsecret";
+  programs.git.config.credential.helper = "manager";
+  environment.systemPackages = [ pkgs.git-credential-manager ];
   system.stateVersion = "25.05"; # just don't change if u don't know what you're doing
 }
